c7628649ff824a3a76e131d038e61e9d03c0d6dd
markusth <25896722+markus7h@users.noreply.github.com>
2022-02-16 11:49:55 +0100

    mth need classes

--- src/GetRating/GetRating.cs
+++ src/GetRating/GetRating.cs
@@ -1,16 +1,11 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System.Net;
-using System.Net.Http;
-using Microsoft.AspNetCore.WebUtilities;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Team5_OH.Function
 {
@@ -18,26 +13,22 @@ namespace Team5_OH.Function
     public static class GetRating
     {
         [FunctionName("GetRating")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            [CosmosDB(
-                databaseName: "ratings", 
-                collectionName: "ratingscollection", 
-                ConnectionStringSetting = "CosmosDbConnectionString")]IAsyncCollector<dynamic> documentsOut,
-            ILogger log)
-        {
-            HttpClient client = new HttpClient();
+        public static async IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetRatings/{ratingId}")] HttpRequest req,
+        [CosmosDB(
+            databaseName: "ratings", 
+            collectionName: "ratingscollection", 
+            ConnectionStringSetting = "CosmosDbConnectionString",
+            SqlQuery = "SELECT * FROM c WHERE c.id={ratingId}")] IEnumerable<RatingInfo> rating,
+        ILogger log)
+        {         
+            if(rating==null || rating.Count()==0)
+            {
+                return new NotFoundObjectResult("No rating found");
+            }
 
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string ratingId = req.Query["ratingId"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-
-            string responseMessage = data;
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(rating.First());
         }
+
     }
 }
